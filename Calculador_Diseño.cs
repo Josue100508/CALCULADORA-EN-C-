6ca59743252a569f8c9d7d9475e0bb2262857230
Josue100508 <josueonancampos@gmail.com>
2024-08-03 10:25:16 -0600

    Calculadora Basica

---
+++ Calculador_Diseño.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CALCULADORA
+{
+    public partial class Calculador_Diseño : Form
+    {
+        public Calculador_Diseño()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculador_Diseño_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
